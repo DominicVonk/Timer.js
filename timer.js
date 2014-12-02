b0b84ffe165347b3bcad5912e038031ebc3679c0
Dominic Vonk <info@projectideal.nl>
2014-12-02 13:41:24 +0100

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,70 @@
+(function($) {
+  $.Timer = function (milliseconds) {
+  var timer = null;
+  if (typeof milliseconds == "undefined") {
+    milliseconds = 0;
+  }
+  var elapsed = null;
+  var running = false;
+  var once = false;
+  return {
+    start: function() {
+      if (running) {
+        window.clearInterval(timer);
+      }
+      if (once) {
+        timer = window.setInterval(function() {
+          elapsed({signalTime: new Date()});
+          window.clearInterval(timer);
+          running = false;
+        }, milliseconds);
+      } else {
+        timer = window.setInterval(function() {
+          elapsed({signalTime: new Date()});
+        }, milliseconds);
+      }
+      running = true;
+    },
+    runOnce: function(_once) {
+      once = _once; 
+      if (running) {
+        if (once) {
+          timer = window.setInterval(function() {
+            elapsed({signalTime: new Date()});
+            window.clearInterval(timer);
+            running = false;
+          }, milliseconds);
+        } else {
+          timer = window.setInterval(function() {
+            elapsed({signalTime: new Date()});
+          }, milliseconds);
+        }
+      }
+    },
+    elapsed: function(func) {
+      elapsed = func;
+      if (running) {
+        window.clearInterval(timer);
+        timer = window.setInterval(function() {
+          elapsed({signalTime: new Date()});
+        }, milliseconds);
+      }
+    },
+    interval: function(_milliseconds) {
+      milliseconds = _milliseconds;
+      if (running) {
+        window.clearInterval(timer);
+        timer = window.setInterval(function() {
+          elapsed({signalTime: new Date()});
+        }, milliseconds);
+      }
+    },
+    stop: function() {
+      if (running) {
+        window.clearInterval(timer);
+      }
+      running = false;
+    }
+  };
+ };
+})(window);
